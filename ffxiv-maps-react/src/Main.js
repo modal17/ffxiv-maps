48cadb6241da8fbcf6b06dd7b77b827d86186193
Modal-d17 <bdt2722@gmail.com>
2018-01-26 02:37:24 -0500

    Fiddled around, set colors of bar.

--- ffxiv-maps-react/src/Main.js
+++ ffxiv-maps-react/src/Main.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { Grid, Dropdown, Checkbox, Container } from 'semantic-ui-react'
 import { mobOptions, floorOptions } from './utils'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine } from 'recharts'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine, LabelList } from 'recharts'
 
 const data = [
-	{name: 'visitor', left: -50, right: 50},
-	{name: 'no visitor', left: -90, right: 10}
+	{left: -60, right: 40}
 ]
 
 export default class Main extends Component {
@@ -49,28 +48,37 @@ export default class Main extends Component {
 
 	render() {
 		return (
-			<Container>
-				<Grid padded> 
-					<Grid.Column width={4}>
-					</Grid.Column>
-					<Grid.Column width={3}>
-						<Dropdown placeholder='Select Floor' fluid selection options={floorOptions} onChange={this.changeFloor} />
-					</Grid.Column>
-					<Grid.Column width={3}>
-						<Dropdown placeholder='Select Mob' fluid selection options={this.state.floorMobs} onChange={this.changeMob}/>
-					</Grid.Column>
-					<Grid.Column width={2}>
-						<Checkbox label='Guest' onChange={this.toggleGuestVisit}/>
-					</Grid.Column>
-				</Grid>
-				<BarChart width={600} height={300} layout='vertical' data={data} stackOffset="sign">
-					<YAxis dataKey="name" type="category"/>
-					<XAxis type="number" domain={[-100,100]}/>
-					<ReferenceLine y={0} stroke='#000'/>
-					<Bar dataKey="left" fill="#8884d8" stackId="stack" />
-					<Bar dataKey="right" fill="#82ca9d" stackId="stack"/>
-				</BarChart>
-			</Container>
+			<div>
+				<Container>
+					<Grid padded> 
+						<Grid.Column width={4}>
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Dropdown placeholder='Select Floor' fluid selection options={floorOptions} onChange={this.changeFloor} />
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Dropdown placeholder='Select Mob' fluid selection options={this.state.floorMobs} onChange={this.changeMob}/>
+						</Grid.Column>
+						<Grid.Column width={2}>
+							<Checkbox label='Guest' onChange={this.toggleGuestVisit}/>
+						</Grid.Column>
+						<Grid.Row centered column={4}>
+							<BarChart width={800} height={150} layout='vertical' data={data} stackOffset="sign">
+								<YAxis dataKey="name" type="category" hide="true"/>
+								<XAxis type="number" domain={[-100,100]} tickFormatter={(tick)=>{ return Math.abs(tick)+"%" }} padding={{ left: 20, right:20}}/>
+								<Tooltip/>
+								<Legend/>
+								<ReferenceLine y={0} stroke='#000'/>
+								<Bar dataKey="left" fill="#FF6F51" stackId="stack"/>
+								<Bar dataKey="right" fill="#38D196" stackId="stack"/>
+							</BarChart>
+						</Grid.Row>
+						<Grid.Row centered>
+							
+						</Grid.Row>
+					</Grid>
+				</Container>
+			</div>
 		) 
 	}
 }
